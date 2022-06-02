2a1ce4c4ff14291fee31f878456d85370be51e38
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-02 02:07:18 -0400

    Added .gitignore. Solved the Number Mirror problem in C++, Java, and Python.

---
+++ Codechef/self_learning/getting_started/Number_Mirror.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Number_Mirror{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        System.out.println(X);
+    }
+}
---
+++ Codechef/self_learning/getting_started/Number_Mirror.py
@@ -0,0 +1,7 @@
+# https://www.codechef.com/LP0TO101/problems/START01
+
+import sys
+input = sys.stdin.readline
+
+number = input()
+print(number)
