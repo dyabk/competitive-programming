b84e691d32cbb7c5d86d2084fb8dfe9a905c8ad3
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-03 06:54:28 -0400

    Solved the Vaccine Dates challenge

---
+++ Vaccine_Dates.py
@@ -0,0 +1,15 @@
+# https://www.codechef.com/LP1TO201/problems/VDATES
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    D, L, R = map(int, input().split())
+    ans = "Too Late"
+    if D <= R:
+        ans = "Take second dose now"
+    if D < L:
+        ans = "Too Early"
+    print(ans)
