86da8143b492158b3774ffec396ad1f58f7ebcd7
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-03 17:43:34 -0400

    Improved the solution of the Programming Languages problem

--- Programming_Languages.py
+++ Programming_Languages.py
@@ -8,9 +8,12 @@ T = int(input())
 
 for _ in range(T):
     A, B, A1, B1, A2, B2 = map(int, input().split())
+    one = (A1, B1)
+    two = (A2, B2)
+    
     ans = 0
-    if (A, B) == (A1, B1) or (A, B) == (B1, A1):
+    if A in one and B in one:
         ans = 1
-    elif (A, B) == (A2, B2) or (A, B) == (B2, A2):
+    elif A in two and B in two:
         ans = 2
     print(ans)
