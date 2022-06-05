8922efb54c46323342bfade2e4f7f2087a10c9d6
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-04 22:02:18 -0400

    Change the solution of the Magical Doors challenge

--- level_up/strings/Magical_Doors.py
+++ level_up/strings/Magical_Doors.py
@@ -7,8 +7,8 @@ input = sys.stdin.readline
 T = int(input())
 
 for _ in range(T):
-    S = input().rstrip()
-    ans = 1 if S[0] == '0' else 0 
+    S = "1" + input().rstrip()
+    ans = 0 
 
     for i in range(1, len(S)):
         if S[i] != S[i - 1]:
