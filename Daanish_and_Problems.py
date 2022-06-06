ed3da521db6e8279d557f6599a9b869502ce674e
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-06 19:54:38 -0400

    Solve the Daanish and Problems challenge

---
+++ Daanish_and_Problems.py
@@ -0,0 +1,19 @@
+# CodeChef - DIV03 - Daanish and Problems
+# https://www.codechef.com/LP1TO201/problems/DIV03
+
+from sys import stdin
+input = stdin.readline
+T = int(input())
+
+for _ in range(T):
+    A = list(map(int, input().split()))
+    K = int(input())
+
+    for i in range(len(A) - 1, -1, -1):
+        if K > 0:
+            decrement = min(K, A[i])
+            A[i] -= decrement
+            K -= decrement
+        if K == 0 and A[i] != 0:
+            print(i + 1)
+            break
