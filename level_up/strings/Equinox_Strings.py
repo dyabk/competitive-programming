01decdcc1ddc7bfaf6ea1dde8431722bd8027c15
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-06 00:00:45 -0400

    Solve the "Equinox" challenge

---
+++ level_up/strings/Equinox_Strings.py
@@ -0,0 +1,31 @@
+# CodeChef - EQUINOX - Equinox Strings
+# https://www.codechef.com/LP1TO201/problems/EQUINOX
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+word = set("EQUINOX")
+
+for _ in range(T):
+    N, A, B = map(int, input().split())
+
+    sarthak = anuradha = 0
+
+    for i in range(N):
+        S = input().rstrip()[0]
+        if S in word:
+            sarthak += A
+        else:
+            anuradha += B
+
+    ans = "DRAW"
+    if sarthak > anuradha:
+        ans = "SARTHAK"
+    elif sarthak < anuradha:
+        ans = "ANURADHA"
+
+    print(ans)
+        
+
