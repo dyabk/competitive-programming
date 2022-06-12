d0eebc44a49e381f6939dcca4b024ea63d7af143
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-12 18:36:53 -0400

    Add the sliding window solution to the Find Averages of Subarrays problem

--- miscellaneous/sliding_window/find_averages_of_subarrays.py
+++ miscellaneous/sliding_window/find_averages_of_subarrays.py
@@ -1,4 +1,4 @@
-def find_averages_of_subarrays(K, arr):
+def brute_force(K, arr):
     result = []
 
     for i in range(len(arr) - K + 1):
@@ -7,8 +7,23 @@ def find_averages_of_subarrays(K, arr):
 
     return result
 
+def sliding_window(K, arr):
+    result = []
+
+    windowSum, windowStart = 0.0, 0
+
+    for windowEnd in range(len(arr)):
+        windowSum += arr[windowEnd]
+
+        if windowEnd >= K - 1:
+            result.append(windowSum / K)
+            windowSum -= arr[windowStart]
+            windowStart += 1
+
+    return result
+
 def main():
-    result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+    result = sliding_window(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
     print("Averages of subarrays of size K: " + str(result))
 
 main()
