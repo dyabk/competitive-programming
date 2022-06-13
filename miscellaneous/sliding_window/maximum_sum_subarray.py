f7f9e60564fab3e46f67b3ec185f97cb88849aa6
dyabk <dzmitry.yabkevich@outlook.com>
2022-06-12 23:22:53 -0400

    Modify the print statement

--- miscellaneous/sliding_window/maximum_sum_subarray.py
+++ miscellaneous/sliding_window/maximum_sum_subarray.py
@@ -1,23 +1,19 @@
 def max_sub_array_of_size_k(k, arr):
   ans = float('-inf')
-  
   windowStart, windowSum = 0, 0
 
   for windowEnd in range(len(arr)):
     windowSum += arr[windowEnd]
 
-    if windowEnd >= k:
-      windowSum -= arr[windowStart]
-      windowStart += 1
-
-      ans = max(ans, windowSum)
+    if windowEnd >= k - 1:
+        ans = max(ans, windowSum)
+        windowSum -= arr[windowStart]
+        windowStart += 1
 
   return ans
 
 def main():
-    result1 = max_sub_array_of_size_k(k=3, arr=[2, 1, 5, 1, 3, 2])
-    result2 = max_sub_array_of_size_k(k=2, arr=[2, 3, 4, 1, 5])
-    print(result1)
-    print(result2)
+    print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+    print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])))
 
 main()
