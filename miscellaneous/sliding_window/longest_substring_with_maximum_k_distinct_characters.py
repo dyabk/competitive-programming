cc127c39f8b262ea1babacdb6ecec507439a7ed7
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-08-13 19:34:50 -0400

    Solve Longest Substring with Maximum K Distinct Characters again

--- miscellaneous/sliding_window/longest_substring_with_maximum_k_distinct_characters.py
+++ miscellaneous/sliding_window/longest_substring_with_maximum_k_distinct_characters.py
@@ -1,30 +1,33 @@
+from calendar import c
 from collections import Counter
+from re import U
 
 def longest_substring_with_k_distinct(str1, k):
   unique_characters = Counter()
   answer = 0
 
-  windowStart = 0
-  for windowEnd in range(len(str1)):
-    char = str1[windowEnd]
-    if char not in unique_characters:
-      unique_characters[char] = 0
-    unique_characters[char] += 1
+  start, end = 0, 0
 
+  for end in range(len(str1)):
+    character = str1[end]
+
+    if character not in unique_characters:
+      unique_characters[character] = 0
+    unique_characters[character] += 1
     while len(unique_characters) > k:
-      unique_characters[str1[windowStart]] -= 1
-      if unique_characters[str1[windowStart]] == 0:
-        del(unique_characters[str1[windowStart]])
-      windowStart += 1
+      unique_characters[str1[start]] -= 1
+      if unique_characters[str1[start]] == 0:
+        del unique_characters[str1[start]]
+      start += 1
 
-    answer = max(answer, windowEnd - windowStart + 1)
-
+    answer = max(answer, end - start + 1)
   return answer
+
 
 def main():
   print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 2)))
   print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 1)))
   print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 3)))
-
+  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 10)))
 
 main()
