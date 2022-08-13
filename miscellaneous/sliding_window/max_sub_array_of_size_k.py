a10b2a7d2a0385397b13a25b5e6d7941820801ac
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-08-13 18:03:28 -0400

    Re-solve the Maximum Sum Subarray of Size K using sliding window

--- miscellaneous/sliding_window/max_sub_array_of_size_k.py
+++ miscellaneous/sliding_window/max_sub_array_of_size_k.py
@@ -6,22 +6,19 @@ def brute_force(k, arr):
 
   return maxSum
 
-def max_sub_array_of_size_k(k, arr):
-  ans = float('-inf')
-  windowStart, windowSum = 0, 0
+def sliding_window(k, arr):
+  cur_sum, max_sum, window_start = 0, 0, 0
+  for window_end in range(len(arr)):
+    cur_sum += arr[window_end]
+    if window_end >= k - 1:
+      max_sum = max(max_sum, cur_sum)
+      cur_sum -= arr[window_start]
+      window_start += 1
 
-  for windowEnd in range(len(arr)):
-    windowSum += arr[windowEnd]
-
-    if windowEnd >= k - 1:
-        ans = max(ans, windowSum)
-        windowSum -= arr[windowStart]
-        windowStart += 1
-
-  return ans
+  return max_sum
 
 def main():
-    print("Maximum sum of a subarray of size K: " + str(brute_force(3, [2, 1, 5, 1, 3, 2])))
-    print("Maximum sum of a subarray of size K: " + str(brute_force(2, [2, 3, 4, 1, 5])))
+    print("Maximum sum of a subarray of size K: " + str(sliding_window(3, [2, 1, 5, 1, 3, 2])))
+    print("Maximum sum of a subarray of size K: " + str(sliding_window(2, [2, 3, 4, 1, 5])))
 
 main()
