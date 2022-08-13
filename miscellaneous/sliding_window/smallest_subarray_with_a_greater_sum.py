5310fb5fb7758b9fbb11926f5a04bbcab6d286ca
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-08-13 19:19:46 -0400

    Re-solve Smallest Subarray with a Greater Sum

--- miscellaneous/sliding_window/smallest_subarray_with_a_greater_sum.py
+++ miscellaneous/sliding_window/smallest_subarray_with_a_greater_sum.py
@@ -1,27 +1,18 @@
-from math import inf
-
 def smallest_subarray_sum(s, arr):
-    windowStart, windowEnd, windowSum = 0, 0, 0
-    answer = inf
-
-    for windowEnd in range (len(arr)):
-        
-        windowSum += arr[windowEnd]
-
-        while windowSum >= s:
-            answer = min(answer, windowEnd - windowStart + 1)
-            windowSum -= arr[windowStart]
-            windowStart += 1
-
-    if answer == inf:
-        answer = 0
-
+    start, end, _sum, answer = 0, 0, 0, float('inf')
+    for end in range(len(arr)):
+        _sum += arr[end]
+        while _sum >= s:
+            answer = min(answer, end - start + 1)
+            _sum -= arr[start]
+            start += 1
+            
     return answer
+
 
 def main():
     print("Smallest subarray length: " + str(smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])))
     print("Smallest subarray length: " + str(smallest_subarray_sum(7, [2, 1, 5, 2, 8])))
     print("Smallest subarray length: " + str(smallest_subarray_sum(8, [3, 4, 1, 1, 6])))
-    print("Smallest subarray length: " + str(smallest_subarray_sum(8, [2, 1, 5, 2, 3, 2])))
 
 main()
