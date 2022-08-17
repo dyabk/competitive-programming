f51fb116619083808f500e3c1ab3bf8f21dd5d30
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-08-17 15:33:45 -0400

    Solve the Fruits into Baskets problem again

--- miscellaneous/sliding_window/fruits_into_baskets.py
+++ miscellaneous/sliding_window/fruits_into_baskets.py
@@ -1,31 +1,32 @@
 from collections import Counter
 
+
 def fruits_into_baskets(fruits):
-  BASKETS = 2
-  two_types = Counter()
-  answer = 0
+    # TODO: Write your code here
+    NUM_OF_BASKETS = 2
+    baskets = Counter()
+    answer = 0
+    start = 0
 
-  windowStart = 0
-  for windowEnd in range(len(fruits)):
-    char = fruits[windowEnd]
-    
-    if char not in two_types:
-      two_types[char] = 0
-    two_types[char] += 1
+    for end in range(len(fruits)):
+        fruit = fruits[end]
+        if fruit not in baskets:
+            baskets[fruit] = 0
+        baskets[fruit] += 1
+        while len(baskets) > NUM_OF_BASKETS:
+            baskets[fruits[start]] -= 1
+            if baskets[fruits[start]] == 0:
+                del baskets[fruits[start]]
+            start += 1
+        answer = max(answer, end - start + 1)
+    return answer
 
-    while len(two_types) > BASKETS:
-      two_types[fruits[windowStart]] -= 1
-      if two_types[fruits[windowStart]] == 0:
-        del(two_types[fruits[windowStart]])
-      windowStart += 1
-
-    answer = max(answer, windowEnd - windowStart + 1)
-
-  return answer
 
 def main():
-  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'A', 'C'])))
-  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C'])))
+    print("Maximum number of fruits: " +
+          str(fruits_into_baskets(['A', 'B', 'C', 'A', 'C'])))
+    print("Maximum number of fruits: " +
+          str(fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C'])))
 
 
 main()
