2bd1cf12c06f901374d891039118038fd631a9f2
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-08-27 22:30:47 -0400

    Re-format

--- miscellaneous/sliding_window/longest_substring_with_maximum_k_distinct_characters.py
+++ miscellaneous/sliding_window/longest_substring_with_maximum_k_distinct_characters.py
@@ -1,33 +1,38 @@
 from calendar import c
 from collections import Counter
-from re import U
+
 
 def longest_substring_with_k_distinct(str1, k):
-  unique_characters = Counter()
-  answer = 0
+    unique_characters = Counter()
+    answer = 0
 
-  start, end = 0, 0
+    start, end = 0, 0
 
-  for end in range(len(str1)):
-    character = str1[end]
+    for end in range(len(str1)):
+        character = str1[end]
 
-    if character not in unique_characters:
-      unique_characters[character] = 0
-    unique_characters[character] += 1
-    while len(unique_characters) > k:
-      unique_characters[str1[start]] -= 1
-      if unique_characters[str1[start]] == 0:
-        del unique_characters[str1[start]]
-      start += 1
+        if character not in unique_characters:
+            unique_characters[character] = 0
+        unique_characters[character] += 1
+        while len(unique_characters) > k:
+            unique_characters[str1[start]] -= 1
+            if unique_characters[str1[start]] == 0:
+                del unique_characters[str1[start]]
+            start += 1
 
-    answer = max(answer, end - start + 1)
-  return answer
+        answer = max(answer, end - start + 1)
+    return answer
 
 
 def main():
-  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 2)))
-  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 1)))
-  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 3)))
-  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 10)))
+    print("Length of the longest substring: " +
+          str(longest_substring_with_k_distinct("araaci", 2)))
+    print("Length of the longest substring: " +
+          str(longest_substring_with_k_distinct("araaci", 1)))
+    print("Length of the longest substring: " +
+          str(longest_substring_with_k_distinct("cbbebi", 3)))
+    print("Length of the longest substring: " +
+          str(longest_substring_with_k_distinct("cbbebi", 10)))
+
 
 main()
