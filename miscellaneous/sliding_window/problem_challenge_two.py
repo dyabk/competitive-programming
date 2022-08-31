04ce4b45693f39fcb918abeb561222f8a83d0b3a
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-08-31 19:57:46 -0400

    Find a shorter sliding window solution for the problem to find string anagrams

--- miscellaneous/sliding_window/problem_challenge_two.py
+++ miscellaneous/sliding_window/problem_challenge_two.py
@@ -2,27 +2,18 @@ from collections import Counter
 
 
 def find_string_anagrams(str1, pattern):
+    pattern_counter = Counter(pattern)
     result_indexes = []
-    pattern_map = Counter(pattern)
+    m = Counter()
+
     start = 0
-    matched = 0
 
     for end in range(len(str1)):
-        right_chr = str1[end]
-        if right_chr in pattern_map:
-            pattern_map[right_chr] -= 1
-            if pattern_map[right_chr] == 0:
-                matched += 1
-
-        if matched == len(pattern_map):
-            result_indexes.append(start)
-
-        while end - start + 1 >= len(pattern):
-            left_chr = str1[start]
-            if left_chr in pattern_map:
-                if pattern_map[left_chr] == 0:
-                    matched -= 1
-                pattern_map[left_chr] += 1
+        m[str1[end]] += 1
+        if end - start + 1 == len(pattern):
+            if all(pattern_counter[chr] == m[chr] for chr in pattern_counter):
+                result_indexes.append(start)
+            m[str1[start]] -= 1
             start += 1
 
     return result_indexes
