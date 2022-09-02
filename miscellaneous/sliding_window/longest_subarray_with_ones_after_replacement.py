18e1904d3b0e4b8e66f9fbb5ec237fae09e38a3d
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-09-02 03:53:43 -0400

    Solve the Smallest WIndow Containing Substring problem the less verbose way

--- miscellaneous/sliding_window/longest_subarray_with_ones_after_replacement.py
+++ miscellaneous/sliding_window/longest_subarray_with_ones_after_replacement.py
@@ -18,27 +18,28 @@ def only_one_count(arr, k):
 
 
 def length_of_longest_substring(arr, k):
-  binCount = [0, 0]
-  answer = 0
+    binCount = [0, 0]
+    answer = 0
 
-  windowStart = 0
-  for windowEnd in range(len(arr)):
-    digit = arr[windowEnd]
+    windowStart = 0
+    for windowEnd in range(len(arr)):
+        digit = arr[windowEnd]
 
-    binCount[digit] += 1
+        binCount[digit] += 1
 
-    while binCount[0] > k:
-      binCount[arr[windowStart]] -= 1
-      windowStart += 1
+        while binCount[0] > k:
+            binCount[arr[windowStart]] -= 1
+            windowStart += 1
 
-    answer = max(answer, windowEnd - windowStart + 1)
+        answer = max(answer, windowEnd - windowStart + 1)
 
-  return answer
+    return answer
+
 
 def main():
-  print(only_one_count([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
-  print(only_one_count(
-    [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
+    print(only_one_count([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
+    print(only_one_count(
+        [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
 
 
 main()
