1f1171b5922981de80812225591ab65f4f77947c
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-09-07 20:15:03 -0400

    Solve the challenge using the hash map solution

--- miscellaneous/two_pointers/pair_with_target_sum.py
+++ miscellaneous/two_pointers/pair_with_target_sum.py
@@ -1,3 +1,14 @@
+def alternate_solution(arr, target_sum):
+    nums = {}
+    for i, num in enumerate(arr):
+        if target_sum - num in nums:
+            return [nums[target_sum - num], i]
+        else:
+            nums[arr[i]] = i
+
+    return [-1, -1]
+
+
 def pair_with_targetsum(arr, target_sum):
     l, r = 0, len(arr) - 1
     answer = [-1, -1]
@@ -16,8 +27,8 @@ def pair_with_targetsum(arr, target_sum):
 
 
 def main():
-    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
-    print(pair_with_targetsum([2, 5, 9, 11], 11))
+    print(alternate_solution([1, 2, 3, 4, 6], 6))
+    print(alternate_solution([2, 5, 9, 11], 11))
 
 
 main()
