c92536ed4b8c9b6639377a1690b036dd63b02daa
dzmitry-dimitri <dzmitry.yabkevich@outlook.com>
2022-10-16 19:35:12 -0400

    Simplify the last solution

--- LeetCode/maximum_consecutive_ones.py
+++ LeetCode/maximum_consecutive_ones.py
@@ -2,19 +2,14 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         i, flip, curRes, maxRes = 0, 0, 0, 0
         for j in range(len(nums)):
-            if nums[j] == 1:
-                curRes += 1
-                maxRes = max(maxRes, curRes)
-            else:
+            if nums[j] == 0:
                 while flip >= k:
-                    if nums[i] == 1:
-                        curRes -= 1
-                    elif nums[i] == 0:
-                        curRes -= 1
+                    if nums[i] == 0:
                         flip -= 1
+                    curRes -= 1
                     i += 1
                 flip += 1
-                curRes += 1
-                maxRes = max(maxRes, curRes)
+            curRes += 1
+            maxRes = max(maxRes, curRes)
 
         return maxRes
